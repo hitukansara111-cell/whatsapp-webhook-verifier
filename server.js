a4f9e4bcf39952f11bb8d463488579708e31a305
hitukansara111-cell <hitukansara111@gmail.com>
2026-07-02 16:12:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ app.post("/webhook/whatsapp", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+  console.log('Server running on port ${PORT}');
 });
